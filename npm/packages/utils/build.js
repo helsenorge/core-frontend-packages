2e3b5d3e8a0b5dec71629505e63adfb7c0fa3a42
Magnus Aga <magnusaga@gmail.com>
2020-03-03 10:13:04 +0100

    fjerner utils fra fw

--- npm/packages/utils/build.js
+++ npm/packages/utils/build.js
@@ -8,6 +8,7 @@ const rimraf = require('rimraf');
 //   // it will be copied if return true
 //   return !src.includes("__tests__")
 // }
+const utilsFiles = ['src/types/dev.d.ts', 'src/types/tabbable.d.ts', 'src/utils/_variables.scss', 'src/types/_variables.scss.d.ts'];
 
 rimraf('./lib', () => {
   exec('tsc');
@@ -16,7 +17,7 @@ rimraf('./lib', () => {
 
   //   console.log('success!')
   // }) // copies file
-
+  fs.copySync('src/types/dev.d.ts', './lib/dev.d.ts');
   fs.copySync('./package.json', './lib/package.json');
   fs.copySync('./.npmrc', './lib/.npmrc');
   fs.copySync('./readme.md', './lib/readme.md');
