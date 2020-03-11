132d13bbe14b49e5279d902f007eecf4ebbf653c
Magnus Aga <magnusaga@gmail.com>
2020-03-11 12:22:02 +0100

    tok bort comments

--- npm/packages/utils/build.js
+++ npm/packages/utils/build.js
@@ -2,21 +2,11 @@ const fs = require('fs-extra');
 const { exec } = require('child_process');
 const rimraf = require('rimraf');
 
-// const filterFunc = (src, dest) => {
-//   console.log(src)
-//   // your logic here
-//   // it will be copied if return true
-//   return !src.includes("__tests__")
-// }
 const utilsFiles = ['src/types/dev.d.ts', 'src/types/tabbable.d.ts', 'src/utils/_variables.scss', 'src/types/_variables.scss.d.ts'];
 
 rimraf('./lib', () => {
   exec('tsc');
-  // fs.copy('./src', './lib', { filter: filterFunc }, err => {
-  //   if (err) return console.error(err)
 
-  //   console.log('success!')
-  // }) // copies file
   fs.copySync('src/types/dev.d.ts', './lib/dev.d.ts');
   fs.copySync('./package.json', './lib/package.json');
   fs.copySync('./.npmrc', './lib/.npmrc');
