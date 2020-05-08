627917d7255beaa901040996d940d1caaafea63c
Marion Le Pontois <marion.le.pontois@ehelse.no>
2020-05-08 14:42:02 +0200

    Merge branch 'master' of https://tfs.ehelse.no/Ehelse/Borgerportalen/_git/HN-Core-Frontend into mlp/tabbable-utils

--- npm/packages/utils/src/__tests__/feature-toggle-test.ts
+++ npm/packages/utils/src/__tests__/feature-toggle-test.ts
@@ -1,26 +1,69 @@
 import getFeatureToggle from '../feature-toggle';
 
-window.HN = window.HN || {};
-window.HN.Commands = window.HN.Commands || {};
-window.HN.Commands.__GetFeatureToggles__ = window.HN.Commands.__GetFeatureToggles__ || {};
-window.HN.Commands.__GetFeatureToggles__.FeatureToggles = window.HN.Commands.__GetFeatureToggles__.FeatureToggles || {};
+function setupFeature(enabled: boolean, authorized: boolean, featureName?: string): void {
+  window.HN = {};
+  window.HN.Rest = {};
+  window.HN.Rest.__Authorized__ = authorized;
+  window.HN.Commands = {};
+  window.HN.Commands.__GetFeatureToggles__ = {};
+  window.HN.Commands.__GetFeatureToggles__.FeatureToggles = {};
+  window.HN.PortalCommands = {};
+  window.HN.PortalCommands.__GetFeatureToggles__ = {};
+  window.HN.PortalCommands.__GetFeatureToggles__.FeatureToggles = {};
 
-function setupFeature(featureName: string, enabled: boolean): void {
-  window.HN.Commands.__GetFeatureToggles__.FeatureToggles[featureName] = enabled;
+  if (featureName) {
+    if (authorized) {
+      window.HN.Commands.__GetFeatureToggles__.FeatureToggles[featureName] = enabled;
+    } else {
+      window.HN.PortalCommands.__GetFeatureToggles__.FeatureToggles[featureName] = enabled;
+    }
+  }
 }
 
 describe('FeatureToggle', () => {
-  it('Should return false if feature is undefined', () => {
-    expect(getFeatureToggle('Feature1')).toBe(false);
+  describe('Gitt at bruker er pålogget', () => {
+    describe('Når Feature1 er undefined', () => {
+      it('Så gis false', () => {
+        setupFeature(false, true);
+        expect(getFeatureToggle('Feature1')).toBe(false);
+      });
+    });
+
+    describe('Når Feature1 er slått på', () => {
+      it('Så gis true', () => {
+        setupFeature(true, true, 'Feature1');
+        expect(getFeatureToggle('Feature1')).toBe(true);
+      });
+    });
+
+    describe('Når Feature2 er slått av', () => {
+      it('Så gis false', () => {
+        setupFeature(false, true, 'Feature2');
+        expect(getFeatureToggle('Feature2')).toBe(false);
+      });
+    });
   });
 
-  it('Should return true if feature is enabled', () => {
-    setupFeature('Feature1', true);
-    expect(getFeatureToggle('Feature1')).toBe(true);
-  });
+  describe('Gitt at bruker ikke er pålogget', () => {
+    describe('Når Feature1 er undefined', () => {
+      it('Så gis false', () => {
+        setupFeature(false, false);
+        expect(getFeatureToggle('Feature1')).toBe(false);
+      });
+    });
 
-  it('Should return false if feature is disabled', () => {
-    setupFeature('Feature2', false);
-    expect(getFeatureToggle('Feature2')).toBe(false);
+    describe('Når Feature1 er slått på', () => {
+      it('Så gis true', () => {
+        setupFeature(true, false, 'Feature1');
+        expect(getFeatureToggle('Feature1')).toBe(true);
+      });
+    });
+
+    describe('Når Feature2 er slått av', () => {
+      it('Så gis false', () => {
+        setupFeature(false, false, 'Feature2');
+        expect(getFeatureToggle('Feature2')).toBe(false);
+      });
+    });
   });
 });
