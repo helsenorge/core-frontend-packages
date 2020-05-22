7970986f334bb33e6327c1a94f1e0817ec131036
SotService <SotService@ehelse.no>
2020-05-22 12:52:01 +0000

    SOT Service update

--- npm/packages/utils/src/types/Resources.d.ts
+++ npm/packages/utils/src/types/Resources.d.ts
@@ -229,6 +229,10 @@ export type HNCoreFrontendMicrowebHeaderFooter = {
   * Kontaktinformasjon
   */
   headerMenuProfileLinksKontaktinformasjonTitle: string;
+  /** 
+  * /kontaktinformasjon
+  */
+  headerMenuProfileLinksKontaktinformasjonURL: string;
 }
 
 export type HNCoreFrontendNPMPackagesCMS = { 
