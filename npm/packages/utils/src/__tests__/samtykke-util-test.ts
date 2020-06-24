38f17d9d9dd1a93620dce05d449f2ed3709d265d
Marion Le Pontois <marion.le.pontois@ehelse.no>
2020-06-24 10:25:42 +0200

    oppdaterer tester

--- npm/packages/utils/src/__tests__/samtykke-util-test.ts
+++ npm/packages/utils/src/__tests__/samtykke-util-test.ts
@@ -1,18 +1,30 @@
 import {} from 'jest';
-import { hasDigitaleHelsetjenesteSamtykke, hasPasientreiserSamtykke } from '../samtykke-util';
+import { hasDigitaleHelsetjenesteSamtykke, hasPasientreiserSamtykke, getSamtykkeStatus, SamtykkeStatus } from '../samtykke-util';
 import { Samtykke } from '../generated-types/minhelseentities';
 import { PersonvernInnstillingDefinisjonGuids } from '../constants/personvernInnstillingDefinisjonIds';
 
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    HN: any;
+  }
+}
+window.HN = window.HN || {};
+window.HN.User = window.HN.User || {};
+window.HN.Commands = window.HN.Commands || {};
+window.HN.Commands.__GetTjenesterMedTilgang__ = window.HN.Commands.__GetTjenesterMedTilgang__ || {};
+window.HN.Commands.__GetTjenesterMedTilgang__.Samtykker = window.HN.Commands.__GetTjenesterMedTilgang__.Samtykker || [];
+
 describe('SamtykkeUtil', () => {
-  let samtykkeDigitaleHelsetjenester = {
+  const samtykkeDigitaleHelsetjenester = {
     PersonvernInnstillingDefinisjonGuid: PersonvernInnstillingDefinisjonGuids.DigitalHelsetjeneste,
     ErAktiv: true,
   };
-  let samtykkePasientreiser = {
+  const samtykkePasientreiser = {
     PersonvernInnstillingDefinisjonGuid: PersonvernInnstillingDefinisjonGuids.Pasientreiser,
     ErAktiv: true,
   };
-  let samtykker: Array<Samtykke> = [samtykkeDigitaleHelsetjenester as Samtykke, samtykkePasientreiser as Samtykke];
+  const samtykker: Array<Samtykke> = [samtykkeDigitaleHelsetjenester as Samtykke, samtykkePasientreiser as Samtykke];
   it('Should have pasientreise samtykke', () => {
     expect(hasDigitaleHelsetjenesteSamtykke(samtykker)).toBe(true);
   });
@@ -20,15 +32,15 @@ describe('SamtykkeUtil', () => {
     expect(hasPasientreiserSamtykke(samtykker)).toBe(true);
   });
 
-  let samtykkeDigitaleHelsetjenesterIkkeAktiv = {
+  const samtykkeDigitaleHelsetjenesterIkkeAktiv = {
     PersonvernInnstillingDefinisjonGuid: PersonvernInnstillingDefinisjonGuids.DigitalHelsetjeneste,
     ErAktiv: false,
   };
-  let samtykkePasientreiserIkkeAktiv = {
+  const samtykkePasientreiserIkkeAktiv = {
     PersonvernInnstillingDefinisjonGuid: PersonvernInnstillingDefinisjonGuids.Pasientreiser,
     ErAktiv: false,
   };
-  let samtykkerIkkeAktiv: Array<Samtykke> = [
+  const samtykkerIkkeAktiv: Array<Samtykke> = [
     samtykkeDigitaleHelsetjenesterIkkeAktiv as Samtykke,
     samtykkePasientreiserIkkeAktiv as Samtykke,
   ];
@@ -39,15 +51,75 @@ describe('SamtykkeUtil', () => {
     expect(hasPasientreiserSamtykke(samtykkerIkkeAktiv)).toBe(false);
   });
 
-  let samtykkeOpplysninger = {
+  const samtykkeOpplysninger = {
     PersonvernInnstillingDefinisjonGuid: PersonvernInnstillingDefinisjonGuids.InnsynIOpplysningerRegistrertOmMeg,
     ErAktiv: true,
   };
-  let samtykkerOpplysninger: Array<Samtykke> = [samtykkeOpplysninger as Samtykke];
+  const samtykkerOpplysninger: Array<Samtykke> = [samtykkeOpplysninger as Samtykke];
   it('Should not have pasientreise samtykke', () => {
     expect(hasDigitaleHelsetjenesteSamtykke(samtykkerOpplysninger)).toBe(false);
   });
   it('Should not have digitale helsetjenester samtykke', () => {
     expect(hasPasientreiserSamtykke(samtykkerOpplysninger)).toBe(false);
+  });
+
+  describe('Gitt at samtykkestatus skal hentes', () => {
+    describe('Gitt at get-samtykke-status kalles', () => {
+      describe('Når innbygger er ny bruker', () => {
+        it('Så skal status oppgi dette', () => {
+          window.HN.User.__ErRepresentasjon__ = false;
+          expect(getSamtykkeStatus()).toBe(SamtykkeStatus.NyBruker);
+        });
+      });
+      describe('Når innbygger er ny bruker og representert', () => {
+        it('Så skal status oppgi dette', () => {
+          window.HN.User.__ErRepresentasjon__ = true;
+          expect(getSamtykkeStatus()).toBe(SamtykkeStatus.ReprNyBruker);
+        });
+      });
+      describe('Når Bruker opprettet før konvertering med samtykke PHA, men har ikke blitt informert.', () => {
+        it('Så skal status oppgi dette, Vis info, ingen samtykkeflyt', () => {
+          window.HN.User.__ErRepresentasjon__ = false;
+          window.HN.User.__SamtykkekonverteringInformert__ = false;
+          window.HN.Commands.__GetTjenesterMedTilgang__.Samtykker = [
+            {
+              PersonvernInnstillingDefinisjonGuid: PersonvernInnstillingDefinisjonGuids.InnsynIOpplysningerRegistrertOmMeg,
+              ErAktiv: true,
+            },
+            {
+              PersonvernInnstillingDefinisjonGuid: PersonvernInnstillingDefinisjonGuids.Pha,
+              ErAktiv: true,
+            },
+          ];
+          expect(getSamtykkeStatus()).toBe(SamtykkeStatus.Dialog);
+        });
+      });
+      describe('Når Bruker er mellom 12 og 16', () => {
+        it('Så skal status oppgi dette,', () => {
+          window.HN.User.__ErRepresentasjon__ = false;
+          window.HN.User.__ErMellom12Og16__ = true;
+          window.HN.Commands.__GetTjenesterMedTilgang__.Samtykker = [
+            {
+              PersonvernInnstillingDefinisjonGuid: PersonvernInnstillingDefinisjonGuids.InnsynIOpplysningerRegistrertOmMeg,
+              ErAktiv: false,
+            },
+          ];
+          expect(getSamtykkeStatus()).toBe(SamtykkeStatus.ErMellom12Og16);
+        });
+      });
+      describe('Når Bruker er mellom 12 og 16', () => {
+        it('Så skal status oppgi dette,', () => {
+          window.HN.User.__ErRepresentasjon__ = false;
+          window.HN.User.__ErMellom12Og16__ = false;
+          window.HN.Commands.__GetTjenesterMedTilgang__.Samtykker = [
+            {
+              PersonvernInnstillingDefinisjonGuid: PersonvernInnstillingDefinisjonGuids.Bruksvilkar,
+              ErAktiv: true,
+            },
+          ];
+          expect(getSamtykkeStatus()).toBe(SamtykkeStatus.Bruksvilkar);
+        });
+      });
+    });
   });
 });
