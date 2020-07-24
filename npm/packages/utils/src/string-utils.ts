ef0f77a6ae9982fb2fdd2bfbb20e71e49e70f2a9
Marion Le Pontois <marion.le.pontois@ehelse.no>
2020-07-24 09:42:42 +0200

    adding string utils to existing file

--- npm/packages/utils/src/string-utils.ts
+++ npm/packages/utils/src/string-utils.ts
@@ -1,3 +1,4 @@
+// Checks if a string is either null, undefined or an empty string
 export function isEmpty(string: string | undefined | null): boolean {
   if (string && string.trim) string = string.trim();
   return string === '' || string === null || string === undefined;
@@ -7,14 +8,18 @@ export function hasInvalidCharacters(string: string): boolean {
   return /[""!#¤%=?`´^¨~*:;£${[\]}|§€><\|]/.test(string);
 }
 
+// Sets the first character as Uppercase
 export function capitalize(capitalizeMe: string): string {
   const capitalized: string = capitalizeMe.charAt(0).toUpperCase() + capitalizeMe.substring(1);
   return capitalized;
 }
+
+// Sets the first character as Lowercase
 export function decapitalize(decapitalizeMe: string): string {
   const decapitalized: string = decapitalizeMe.charAt(0).toLowerCase() + decapitalizeMe.substring(1);
   return decapitalized;
 }
+
 // Replaces items in string with format {number} ex: {2}, starting from 0
 // Paramtere args is a array in order you want them replaced ex: ['foo', 'bar']
 // Returns a new string where values has been replaced
