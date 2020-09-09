c7549afd44c7972e1cfb7eff6f2bd4a6a54e302c
Marion Le Pontois <marion.le.pontois@ehelse.no>
2020-09-09 08:52:26 +0200

    legger til forwardRef rundt layout-change HOC

--- npm/packages/utils/src/hoc/layout-change.tsx
+++ npm/packages/utils/src/hoc/layout-change.tsx
@@ -1,7 +1,17 @@
+/*  This file has been converted to TypeScript
+ When making changes to this file please make them
+ in the file with a .ts or .tsx extension located in
+ the Common/src/toolkit folder structure
+ Then run the command "npm run build-toolkit" to generate the jsx file
+ Please check both files into TFS */
+
 import * as React from 'react';
 import LayoutUtils from '../layout';
 
-export interface State {
+export interface LayoutState {
+  oneToTwoColumn?: boolean;
+  twoToThreeColumn?: boolean;
+  threeTwoFourColumn?: boolean;
   nullToXs?: boolean;
   xsToSm?: boolean;
   smToMd?: boolean;
@@ -9,7 +19,10 @@ export interface State {
   lgToXl?: boolean;
 }
 
-export interface Props {
+export interface OriginalProps {
+  oneToTwoColumn?: boolean;
+  twoToThreeColumn?: boolean;
+  threeTwoFourColumn?: boolean;
   nullToXs?: boolean;
   xsToSm?: boolean;
   smToMd?: boolean;
@@ -17,11 +30,18 @@ export interface Props {
   lgToXl?: boolean;
 }
 
-export default function layoutChange<T>(COMPONENT: React.ComponentClass<T & Props> | React.FC<T & Props>): React.ComponentClass<T> {
-  class LayoutChangeComponent extends React.Component<T, State> {
-    constructor(props: T) {
+export default function layoutChange<T extends React.Component, OriginalProps>(
+  COMPONENT: React.ComponentClass<OriginalProps & { ref?: React.RefObject<T> }> | React.FC<OriginalProps & { ref?: React.RefObject<T> }>
+): React.ForwardRefExoticComponent<React.PropsWithoutRef<OriginalProps> & React.RefAttributes<T>> {
+  type PrivateProps = { forwardedRef: React.RefObject<T> };
+  type Props = OriginalProps & PrivateProps;
+  class LayoutChangeComponent extends React.Component<Props, LayoutState> {
+    constructor(props: Props) {
       super(props);
       this.state = {
+        oneToTwoColumn: LayoutUtils.isOneToTwoColumn(),
+        twoToThreeColumn: LayoutUtils.isTwoToThreeColumn(),
+        threeTwoFourColumn: LayoutUtils.isThreeTwoFourColumn(),
         nullToXs: LayoutUtils.isNullToXs(),
         xsToSm: LayoutUtils.isXsToSm(),
         smToMd: LayoutUtils.isSmToMd(),
@@ -54,20 +74,29 @@ export default function layoutChange<T>(COMPONENT: React.ComponentClass<T & Prop
       this.updateState();
     };
 
-    updateState(): void {
+    updateState = (): void => {
       this.setState({
+        oneToTwoColumn: LayoutUtils.isOneToTwoColumn(),
+        twoToThreeColumn: LayoutUtils.isTwoToThreeColumn(),
+        threeTwoFourColumn: LayoutUtils.isThreeTwoFourColumn(),
         nullToXs: LayoutUtils.isNullToXs(),
         xsToSm: LayoutUtils.isXsToSm(),
         smToMd: LayoutUtils.isSmToMd(),
         mdToLg: LayoutUtils.isMdToLg(),
         lgToXl: LayoutUtils.isLgToXl(),
       });
-    }
+    };
 
     render(): JSX.Element {
+      const { forwardedRef, ...originalProps } = this.props;
+
       return (
         <COMPONENT
-          {...this.props}
+          ref={forwardedRef}
+          {...(originalProps as OriginalProps)}
+          oneToTwoColumn={this.state.oneToTwoColumn}
+          twoToThreeColumn={this.state.twoToThreeColumn}
+          threeTwoFourColumn={this.state.threeTwoFourColumn}
           nullToXs={this.state.nullToXs}
           xsToSm={this.state.xsToSm}
           smToMd={this.state.smToMd}
@@ -77,5 +106,8 @@ export default function layoutChange<T>(COMPONENT: React.ComponentClass<T & Prop
       );
     }
   }
-  return LayoutChangeComponent;
+
+  const RefForwardingFactory = (props: OriginalProps, ref: React.RefObject<T>) => <LayoutChangeComponent {...props} forwardedRef={ref} />;
+
+  return React.forwardRef(RefForwardingFactory);
 }
