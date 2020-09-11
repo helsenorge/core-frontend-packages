0090826d48383f65cb2ee85fccabc3ac5752ce9f
Marion Le Pontois <marion.le.pontois@ehelse.no>
2020-09-11 15:14:30 +0200

    tester for hn-user og opprydning

--- npm/packages/utils/src/hn-user.ts
+++ npm/packages/utils/src/hn-user.ts
@@ -1,91 +1,167 @@
 import { SamtykkeLevel, FullmaktType, FullmaktEgenskaper } from './types/entities';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-declare let HN: any;
+declare let HN: {
+  User: {
+    __LastLogOn__: string;
+    __Name__: string;
+    __RepresentedUser__: string;
+    __AvatarColor__: string;
+    __HasRepresentation__: boolean;
+    __ShowWelcome__: boolean;
+    __NewSocialSecurityNumber__: string;
+    __ShowBruksvilkar__: boolean;
+    __Unsupported__: boolean;
+    __ErRepresentasjon__: boolean;
+    __SamtykkekonverteringInformert__: boolean;
+    __RequiredSamtykkeLevel__: SamtykkeLevel;
+    __ErMellom12Og16__: boolean;
+    __IsNavigatingToSignOut__: boolean;
+    __FullmaktEgenskaper__: FullmaktEgenskaper;
+  };
+};
 
 window.HN = window.HN || {};
 window.HN.User = window.HN.User || {};
 
-export function getLastLogOn(): string {
+/**
+ * Returnerer verdien satt på HN User __LastLogOn__
+ */
+export const getLastLogOn = (): string => {
   return HN.User.__LastLogOn__;
-}
+};
 
-export function getName(): string {
+/**
+ * Returnerer verdien satt på HN User __Name__
+ */
+export const getName = (): string => {
   return HN.User.__Name__;
-}
+};
 
-export function getRepresentedUser(): string {
+/**
+ * Returnerer verdien satt på HN User __RepresentedUser__
+ */
+export const getRepresentedUser = (): string => {
   return HN.User.__RepresentedUser__;
-}
+};
 
-export function getAvatarColor(): number {
+/**
+ * Returnerer verdien satt på HN User __AvatarColor__
+ */
+export const getAvatarColor = (): string => {
   return HN.User.__AvatarColor__;
-}
+};
 
-export function getHasRepresentation(): boolean {
+/**
+ * Returnerer verdien satt på HN User __HasRepresentation__
+ */
+export const getHasRepresentation = (): boolean => {
   return HN.User.__HasRepresentation__;
-}
+};
 
-export function getShowWelcome(): boolean {
+/**
+ * Returnerer verdien satt på HN User __ShowWelcome__
+ */
+export const getShowWelcome = (): boolean => {
   return HN.User.__ShowWelcome__;
-}
+};
 
-export function getNewSocialSecurityNumber(): boolean {
+/**
+ * Returnerer verdien satt på HN User __NewSocialSecurityNumber__
+ */
+export const getNewSocialSecurityNumber = (): string => {
   return HN.User.__NewSocialSecurityNumber__;
-}
+};
 
-export function getShowBruksvilkar(): boolean {
+/**
+ * Returnerer verdien satt på HN User __ShowBruksvilkar__
+ */
+export const getShowBruksvilkar = (): boolean => {
   return HN.User.__ShowBruksvilkar__;
-}
+};
 
-export function getUnsupported(): boolean {
+/**
+ * Returnerer verdien satt på HN User __Unsupported__
+ */
+export const getUnsupported = (): boolean => {
   return HN.User.__Unsupported__;
-}
+};
 
-export function getErRepresentasjon(): boolean {
+/**
+ * Returnerer verdien satt på HN User __ErRepresentasjon__
+ */
+export const getErRepresentasjon = (): boolean => {
   return HN.User.__ErRepresentasjon__;
-}
+};
 
-export function getSamtykkekonverteringInformert(): boolean {
+/**
+ * Returnerer verdien satt på HN User __SamtykkekonverteringInformert__
+ */
+export const getSamtykkekonverteringInformert = (): boolean => {
   return HN.User.__SamtykkekonverteringInformert__;
-}
+};
 
-export function setTempSamtykkekonverteringInformert(value: boolean): void {
+/**
+ * Setter ny verdi på HN User __SamtykkekonverteringInformert__
+ * @param value - verdi som settes
+ */
+export const setTempSamtykkekonverteringInformert = (value: boolean): void => {
   HN.User.__SamtykkekonverteringInformert__ = value;
-}
+};
 
-export function getErMellom12Og16(): boolean {
+/**
+ * Returnerer verdien satt på HN User __ErMellom12Og16__
+ */
+export const getErMellom12Og16 = (): boolean => {
   return HN.User.__ErMellom12Og16__;
-}
+};
 
-export function erForeldrerepresentasjon12Pluss(): boolean {
+/**
+ * Returnerer true når __ErMellom12Og16__ og __HasRepresentation__ er true (og fullmakt er null)
+ */
+export const erForeldrerepresentasjon12Pluss = (): boolean => {
   return HN.User.__ErMellom12Og16__ && HN.User.__FullmaktEgenskaper__ === null && HN.User.__HasRepresentation__ === true;
-}
+};
 
-export function getRequiredSamtykkeLevel(): SamtykkeLevel {
+/**
+ * Returnerer verdien satt på HN User __RequiredSamtykkeLevel__
+ */
+export const getRequiredSamtykkeLevel = (): SamtykkeLevel => {
   return HN.User.__RequiredSamtykkeLevel__;
-}
+};
 
-export function getIsNavigatingToSignOut(): SamtykkeLevel {
+/**
+ * Returnerer verdien satt på HN User __IsNavigatingToSignOut__
+ */
+export const getIsNavigatingToSignOut = (): boolean => {
   return HN.User.__IsNavigatingToSignOut__;
-}
+};
 
-export function setIsNavigatingToSignOut(value: boolean): void {
+/**
+ * Setter ny verdi på HN User __IsNavigatingToSignOut__
+ * @param value - verdi som settes
+ */
+export const setIsNavigatingToSignOut = (value: boolean): void => {
   HN.User.__IsNavigatingToSignOut__ = value;
-}
+};
 
-export function getErOrdinaerAnalogFullmakt(): boolean {
+/**
+ * Returnerer true når __FullmaktEgenskaper__ er analog og ordinær
+ */
+export const getErOrdinaerAnalogFullmakt = (): boolean => {
   const egenskaper: FullmaktEgenskaper = HN.User.__FullmaktEgenskaper__;
   if (egenskaper && egenskaper.Analog) {
     if (egenskaper.FullmaktType === FullmaktType.OrdinaerFullmakt) return true;
   }
   return false;
-}
+};
 
-export function getErIkkeSamtykkeKompetentFullmakt(): boolean {
+/**
+ * Returnerer true når __FullmaktEgenskaper__ er analog og UtenSamtykkekompenanseOver12
+ */
+export const getErIkkeSamtykkeKompetentFullmakt = (): boolean => {
   const egenskaper: FullmaktEgenskaper = HN.User.__FullmaktEgenskaper__;
   if (egenskaper && egenskaper.Analog) {
     if (egenskaper.FullmaktType === FullmaktType.UtenSamtykkekompenanseOver12) return true;
   }
   return false;
-}
+};
