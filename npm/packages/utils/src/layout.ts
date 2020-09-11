871ac5ee531356e68a4cbf2b4afa428144412e5d
Marion Le Pontois <marion.le.pontois@ehelse.no>
2020-09-11 14:23:00 +0200

    Merge branch 'master' of https://tfs.ehelse.no/Ehelse/Borgerportalen/_git/HN-Core-Frontend into mlp/core-utils

--- npm/packages/utils/src/layout.ts
+++ npm/packages/utils/src/layout.ts
@@ -1,35 +1,19 @@
-/*  This file has been converted to TypeScript
- When making changes to this file please make them
- in the file with a .ts or .tsx extension located in
- the Common/src/toolkit folder structure
- Then run the command "npm run build-toolkit" to generate the jsx file
- Please check both files into TFS */
-
-import LayoutConstants from './constants/layout';
+import { theme } from '@helsenorge/designsystem-react';
 
 export default {
-  isOneToTwoColumn: function(): boolean {
-    return window.matchMedia(`screen and (max-width: ${LayoutConstants.ONE_TO_TWO_COLUMN})`).matches;
-  },
-  isTwoToThreeColumn: function(): boolean {
-    return window.matchMedia(`screen and (max-width: ${LayoutConstants.TWO_TO_THREE_COLUMN})`).matches;
-  },
-  isThreeTwoFourColumn: function(): boolean {
-    return window.matchMedia(`screen and (max-width: ${LayoutConstants.THREE_TO_FOUR_COLUMN})`).matches;
-  },
   isNullToXs: function(): boolean {
-    return window.matchMedia(`screen and (max-width: ${LayoutConstants.NULL_TO_XS})`).matches;
+    return window.matchMedia(`screen and (max-width: ${theme.breakpoints.null})`).matches;
   },
   isXsToSm: function(): boolean {
-    return window.matchMedia(`screen and (max-width: ${LayoutConstants.XS_TO_SM})`).matches;
+    return window.matchMedia(`screen and (max-width: ${theme.breakpoints.sm})`).matches;
   },
   isSmToMd: function(): boolean {
-    return window.matchMedia(`screen and (max-width: ${LayoutConstants.SM_TO_MD})`).matches;
+    return window.matchMedia(`screen and (max-width: ${theme.breakpoints.md})`).matches;
   },
   isMdToLg: function(): boolean {
-    return window.matchMedia(`screen and (max-width: ${LayoutConstants.MD_TO_LG})`).matches;
+    return window.matchMedia(`screen and (max-width: ${theme.breakpoints.lg})`).matches;
   },
   isLgToXl: function(): boolean {
-    return window.matchMedia(`screen and (max-width: ${LayoutConstants.LG_TO_XL})`).matches;
+    return window.matchMedia(`screen and (max-width: ${theme.breakpoints.xl})`).matches;
   },
 };
--- npm/packages/utils/src/string-utils.ts
+++ npm/packages/utils/src/string-utils.ts
@@ -39,3 +39,8 @@ export default {
   decapitalize,
   format,
 };
+
+export function isNorwegianPhoneNumber(phoneNumber: string | number): boolean {
+  const regexString = '^(0047|[+]47)?[4|9][0-9]{7,7}$';
+  return new RegExp(regexString, 'i').test(phoneNumber.toString().replace(new RegExp(' ', 'g'), ''));
+}
