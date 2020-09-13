0d1341c16f515e9ad88d8cb25cd7ab9bfea36516
Marion Le Pontois <marion.le.pontois@ehelse.no>
2020-09-13 18:37:58 +0200

    lager enhetstester for logger

---
+++ npm/packages/utils/src/__tests__/logger.spec.ts
@@ -0,0 +1,200 @@
+import loggerUtils from '../logger';
+
+describe('Logger', () => {
+  const originalEnv = process.env['NODE_ENV'];
+  process.env['NODE_ENV'] = 'dev';
+  const originalConsole = global.console;
+  // adding console functions that are undefined
+  console.exception = () => {};
+  console.profile = () => {};
+  console.profileEnd = () => {};
+
+  const logToServerMock = jest.spyOn(loggerUtils, 'logToServer');
+
+  describe('Gitt at process.env.NODE_ENV er ulik prod', () => {
+    describe('Når assert kalles', () => {
+      it('Så logges det riktig', () => {
+        const consoleMock = jest.spyOn(global.console, 'assert');
+        loggerUtils.assert(true, 'testmessage', 'param1');
+        expect(consoleMock.mock.calls[0][1]).toEqual('testmessage');
+      });
+    });
+
+    describe('Når clear kalles', () => {
+      it('Så logges det riktig', () => {
+        const consoleMock = jest.spyOn(global.console, 'clear');
+        loggerUtils.clear();
+        expect(consoleMock).toHaveBeenCalled();
+      });
+    });
+
+    describe('Når count kalles', () => {
+      it('Så logges det riktig', () => {
+        const consoleMock = jest.spyOn(global.console, 'count');
+        loggerUtils.count('2');
+        expect(consoleMock.mock.calls[0]).toEqual(['2']);
+      });
+    });
+
+    describe('Når debug kalles', () => {
+      it('Så logges det riktig', () => {
+        const consoleMock = jest.spyOn(global.console, 'debug');
+
+        loggerUtils.debug('message', 'blabla');
+        expect(consoleMock.mock.calls[0][0]).toEqual('message');
+        expect(consoleMock.mock.calls[0][1]).toEqual(['blabla']);
+        expect(logToServerMock).toHaveBeenCalledWith(1, 'message', ['blabla']);
+      });
+    });
+
+    describe('Når dir kalles', () => {
+      it('Så logges det riktig', () => {
+        const consoleMock = jest.spyOn(global.console, 'dir');
+
+        loggerUtils.dir('value', 'param');
+        expect(consoleMock.mock.calls[0][0]).toEqual('value');
+        expect(consoleMock.mock.calls[0][1]).toEqual(['param']);
+      });
+    });
+
+    describe('Når dirxml kalles', () => {
+      it('Så logges det riktig', () => {
+        const consoleMock = jest.spyOn(global.console, 'dirxml');
+
+        loggerUtils.dirxml('dirxml value');
+        expect(consoleMock.mock.calls[0][0]).toEqual('dirxml value');
+      });
+    });
+
+    describe('Når error kalles', () => {
+      it('Så logges det riktig', () => {
+        const consoleMock = jest.spyOn(global.console, 'error');
+        try {
+          loggerUtils.error('error message', 'error blabla');
+        } catch (e) {
+          expect(consoleMock.mock.calls[0][0]).toEqual('error message');
+          expect(consoleMock.mock.calls[0][1]).toEqual(['error blabla']);
+          expect(logToServerMock.mock.calls[1][0]).toEqual(['error message']);
+        }
+      });
+    });
+
+    describe('Når exception kalles', () => {
+      it('Så logges det riktig', () => {
+        const consoleMock = jest.spyOn(global.console, 'exception');
+        try {
+          loggerUtils.exception('exception message', 'exception blabla');
+        } catch (e) {
+          expect(consoleMock.mock.calls[0][0]).toEqual('exception message');
+          expect(consoleMock.mock.calls[0][1]).toEqual(['exception blabla']);
+          expect(logToServerMock.mock.calls[2][0]).toEqual(['exception message']);
+        }
+      });
+    });
+
+    describe('Når group kalles', () => {
+      it('Så logges det riktig', () => {
+        const consoleMock = jest.spyOn(global.console, 'group');
+        loggerUtils.group('group');
+        expect(consoleMock.mock.calls[0]).toEqual(['group']);
+      });
+    });
+
+    describe('Når groupCollapsed kalles', () => {
+      it('Så logges det riktig', () => {
+        const consoleMock = jest.spyOn(global.console, 'groupCollapsed');
+        loggerUtils.groupCollapsed('groupCollapsed');
+        expect(consoleMock.mock.calls[0]).toEqual(['groupCollapsed']);
+      });
+    });
+
+    describe('Når groupEnd kalles', () => {
+      it('Så logges det riktig', () => {
+        const consoleMock = jest.spyOn(global.console, 'groupEnd');
+        loggerUtils.groupEnd();
+        expect(consoleMock).toHaveBeenCalled();
+      });
+    });
+
+    describe('Når info kalles', () => {
+      it('Så logges det riktig', () => {
+        const consoleMock = jest.spyOn(global.console, 'info');
+        loggerUtils.info('info message', 'info blabla');
+        expect(consoleMock.mock.calls[0][0]).toEqual('info message');
+        expect(consoleMock.mock.calls[0][1]).toEqual(['info blabla']);
+        expect(logToServerMock.mock.calls[3][1]).toEqual('info message');
+      });
+    });
+
+    describe('Når log kalles', () => {
+      it('Så logges det riktig', () => {
+        const consoleMock = jest.spyOn(global.console, 'log');
+        loggerUtils.log('log message', 'log blabla');
+        expect(consoleMock.mock.calls[0][0]).toEqual('log message');
+        expect(consoleMock.mock.calls[0][1]).toEqual(['log blabla']);
+      });
+    });
+
+    describe('Når profile kalles', () => {
+      it('Så logges det riktig', () => {
+        const consoleMock = jest.spyOn(global.console, 'profile');
+        loggerUtils.profile('profile');
+        expect(consoleMock.mock.calls[0]).toEqual(['profile']);
+      });
+    });
+
+    describe('Når profileEnd kalles', () => {
+      it('Så logges det riktig', () => {
+        const consoleMock = jest.spyOn(global.console, 'profileEnd');
+        loggerUtils.profileEnd();
+        expect(consoleMock).toHaveBeenCalled();
+      });
+    });
+
+    describe('Når table kalles', () => {
+      it('Så logges det riktig', () => {
+        const consoleMock = jest.spyOn(global.console, 'table');
+        loggerUtils.table(['data']);
+        expect(consoleMock.mock.calls[0][0][0]).toEqual(['data']);
+      });
+    });
+
+    describe('Når time kalles', () => {
+      it('Så logges det riktig', () => {
+        const consoleMock = jest.spyOn(global.console, 'time');
+        loggerUtils.time('timername');
+        expect(consoleMock.mock.calls[0]).toEqual(['timername']);
+      });
+    });
+
+    describe('Når timeEnd kalles', () => {
+      it('Så logges det riktig', () => {
+        const consoleMock = jest.spyOn(global.console, 'timeEnd');
+        loggerUtils.timeEnd('timername');
+        expect(consoleMock.mock.calls[0]).toEqual(['timername']);
+      });
+    });
+
+    describe('Når trace kalles', () => {
+      it('Så logges det riktig', () => {
+        const consoleMock = jest.spyOn(global.console, 'trace');
+        loggerUtils.trace('trace message', 'trace blabla');
+        expect(consoleMock.mock.calls[0][0]).toEqual('trace message');
+        expect(consoleMock.mock.calls[0][1]).toEqual(['trace blabla']);
+        expect(logToServerMock.mock.calls[4][1]).toEqual('trace message');
+      });
+    });
+
+    describe('Når warn kalles', () => {
+      it('Så logges det riktig', () => {
+        const consoleMock = jest.spyOn(global.console, 'warn');
+        loggerUtils.warn('warn message', 'warn blabla');
+        expect(consoleMock.mock.calls[0][0]).toEqual('warn message');
+        expect(consoleMock.mock.calls[0][1]).toEqual(['warn blabla']);
+        expect(logToServerMock.mock.calls[5][1]).toEqual('warn message');
+      });
+    });
+  });
+  process.env['NODE_ENV'] = originalEnv;
+  global.console = originalConsole;
+});
--- npm/packages/utils/src/logger.ts
+++ npm/packages/utils/src/logger.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { postAuto, getPortal, OperationResponse } from './hn-service';
 
 export enum LogLevel {
@@ -25,15 +24,15 @@ interface LogEntry {
 
 const logQueue: LogEntry[] = [];
 
-function processQueue(queue: LogEntry[], serverLogLevel: number) {
+const processQueue = (queue: LogEntry[], serverLogLevel: number): void => {
   const entry = queue.shift();
   if (entry && entry.Level >= serverLogLevel) {
     postAuto('Log', entry).catch();
     processQueue(queue, serverLogLevel);
   }
-}
+};
 
-function SetupLogLevel() {
+const SetupLogLevel = (): void => {
   getPortal('GetEnvironment')
     .then((data: GetEnvironmentOperationResponse) => {
       ServerLogLevel = data.Level;
@@ -42,9 +41,9 @@ function SetupLogLevel() {
     .catch(() => {
       ServerLogLevel = LogLevel.Off;
     });
-}
+};
 
-function unwrapError(potentialError: Error | any[] | string): string {
+const unwrapError = (potentialError: Error | Array<Error | string> | string): string => {
   if (potentialError instanceof Error) {
     return potentialError.message + ' Stack: ' + potentialError.stack;
   } else if (typeof potentialError === 'string' || potentialError instanceof String) {
@@ -55,11 +54,12 @@ function unwrapError(potentialError: Error | any[] | string): string {
   } else {
     return JSON.stringify(potentialError);
   }
-}
+};
 
-function generateEntry(level: LogLevel, message?: string, ...optionalParams: any[]): LogEntry {
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+const generateEntry = (level: LogLevel, message?: string, ...optionalParams: Array<any>): LogEntry => {
   let fullMessage = message;
-  fullMessage = optionalParams.reduce((previous: string, current: Error | any[] | string) => {
+  fullMessage = optionalParams.reduce((previous: string, current: Error | Array<Error | string> | string) => {
     return previous + ' ' + unwrapError(current);
   }, fullMessage);
   return {
@@ -67,18 +67,22 @@ function generateEntry(level: LogLevel, message?: string, ...optionalParams: any
     Message: fullMessage,
     Url: window.location.href,
   };
-}
+};
 
-function logToServer(level: LogLevel, message?: string, ...optionalParams: any[]) {
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+export const logToServer = (level: LogLevel, message?: string, ...optionalParams: any[]): void => {
   if (ServerLogLevel === undefined) {
     SetupLogLevel();
     logQueue.push(generateEntry(level, message, optionalParams));
   } else if (level >= ServerLogLevel) {
     postAuto('Log', generateEntry(level, message, optionalParams)).catch();
   }
-}
+};
 
-export function captureErrors() {
+/**
+ * Logger error til serveren
+ */
+export const captureErrors = (): void => {
   window.addEventListener('error', function(e: ErrorEvent) {
     let message = e.message;
     const error = e.error;
@@ -94,75 +98,145 @@ export function captureErrors() {
         message = message + ' Filename:' + e.filename;
       }
     }
-    logToServer(LogLevel.Error, message);
+    exportFunctions.logToServer(LogLevel.Error, message);
   });
-}
+};
 
-export function assert(test?: boolean, message?: string, ...optionalParams: any[]): void {
+/**
+ * Lager en console.assert
+ * @param test - test som sendes til console assert
+ * @param message - message som sendes til console assert
+ * @param optionalParams params som sendes til console assert
+ */
+export const assert = (test?: boolean, message?: string, ...optionalParams: string[]): void => {
   if (process.env.NODE_ENV !== 'production') {
     console.assert(test, message, optionalParams);
   }
-}
-export function clear(): void {
+};
+
+/**
+ * Lager en console.clear
+ */
+export const clear = (): void => {
   if (process.env.NODE_ENV !== 'production') {
     console.clear();
   }
-}
-export function count(countTitle?: string): void {
+};
+
+/**
+ * Lager en console.count
+ * @param countTitle - string som sendes i console.count
+ */
+export const count = (countTitle?: string): void => {
   if (process.env.NODE_ENV !== 'production') {
     console.count(countTitle);
   }
-}
-export function debug(message?: any, ...optionalParams: any[]): void {
+};
+
+/**
+ * Lager en console.debug og logger til serveren
+ * @param message - string som sendes i console.debug og logges til serveren
+ * @param optionalParams params som sendes til console.debug og logges til serveren
+ */
+export const debug = (message?: string, ...optionalParams: string[]): void => {
   if (process.env.NODE_ENV !== 'production') {
     console.debug(message, optionalParams);
   }
-  logToServer(LogLevel.Debug, message, optionalParams);
-}
-export function dir(value?: any, ...optionalParams: any[]): void {
+  exportFunctions.logToServer(LogLevel.Debug, message, optionalParams);
+};
+
+/**
+ * Lager en console.dir
+ * @param value - string som sendes i console.dir og logges til serveren
+ * @param optionalParams params som sendes til console.dir og logges til serveren
+ */
+export const dir = (value?: unknown, ...optionalParams: string[]): void => {
   if (process.env.NODE_ENV !== 'production') {
     console.dir(value, optionalParams);
   }
-}
-export function dirxml(value: any): void {
+};
+
+/**
+ * Lager en console.dirxml
+ * @param value - string som sendes i console.dirxml og logges til serveren
+ */
+export const dirxml = (value: unknown): void => {
   if (process.env.NODE_ENV !== 'production') {
     console.dirxml(value);
   }
-}
-export function error(message?: any, ...optionalParams: any[]): void {
+};
+
+/**
+ * Lager en console.error og logger til serveren
+ * @param message - string som sendes i console.error og logges til serveren
+ * @param optionalParams params som sendes til console.error og logges til serveren
+ */
+export const error = (message?: string, ...optionalParams: string[]): void => {
   if (process.env.NODE_ENV !== 'production') {
     console.error(message, optionalParams);
   }
-  logToServer(LogLevel.Error, message, optionalParams);
-}
-export function exception(message?: string, ...optionalParams: any[]): void {
+  exportFunctions.logToServer(LogLevel.Error, message, optionalParams);
+};
+
+/**
+ * Lager en console.exception og logger til serveren
+ * @param message - string som sendes i console.exception og logges til serveren
+ * @param optionalParams params som sendes til console.exception og logges til serveren
+ */
+export const exception = (message?: string, ...optionalParams: string[]): void => {
   if (process.env.NODE_ENV !== 'production') {
     console.exception(message, optionalParams);
   }
-  logToServer(LogLevel.Error, message, optionalParams);
-}
-export function group(groupTitle?: string): void {
+  exportFunctions.logToServer(LogLevel.Error, message, optionalParams);
+};
+
+/**
+ * Lager en console.group
+ * @param groupTitle - string som sendes i console.group
+ */
+export const group = (groupTitle?: string): void => {
   if (process.env.NODE_ENV !== 'production') {
     console.group(groupTitle);
   }
-}
-export function groupCollapsed(groupTitle?: string): void {
+};
+
+/**
+ * Lager en console.groupCollapsed
+ * @param groupTitle - string som sendes i console.groupCollapsed
+ */
+export const groupCollapsed = (groupTitle?: string): void => {
   if (process.env.NODE_ENV !== 'production') {
     console.groupCollapsed(groupTitle);
   }
-}
-export function groupEnd(): void {
+};
+
+/**
+ * Lager en console.groupEnd
+ */
+export const groupEnd = (): void => {
   if (process.env.NODE_ENV !== 'production') {
     console.groupEnd();
   }
-}
-export function info(message?: any, ...optionalParams: any[]): void {
+};
+
+/**
+ * Lager en console.info og logger til serveren
+ * @param message - string som sendes i console.info og logges til serveren
+ * @param optionalParams params som sendes til console.info og logges til serveren
+ */
+export const info = (message?: string, ...optionalParams: string[]): void => {
   if (process.env.NODE_ENV !== 'production') {
     console.info(message, optionalParams);
   }
-  logToServer(LogLevel.Info, message, optionalParams);
-}
-export function log(message?: any, ...optionalParams: any[]): void {
+  exportFunctions.logToServer(LogLevel.Info, message, optionalParams);
+};
+
+/**
+ * Lager en console.log
+ * @param message - string som sendes i console.log
+ * @param optionalParams params som sendes til console.log
+ */
+export const log = (message?: string, ...optionalParams: string[]): void => {
   if (process.env.NODE_ENV !== 'production') {
     if (optionalParams && optionalParams.length > 0) {
       console.log(message, optionalParams);
@@ -170,47 +244,84 @@ export function log(message?: any, ...optionalParams: any[]): void {
       console.log(message);
     }
   }
-}
+};
 
-export function profile(reportName?: string): void {
+/**
+ * Lager en console.profile
+ * @param reportName - string som sendes i console.groupCollapsed
+ */
+export const profile = (reportName?: string): void => {
   if (process.env.NODE_ENV !== 'production') {
     console.profile(reportName);
   }
-}
-export function profileEnd(): void {
+};
+
+/**
+ * Lager en console.profileEnd
+ */
+export const profileEnd = (): void => {
   if (process.env.NODE_ENV !== 'production') {
     console.profileEnd();
   }
-}
-export function table(...data: any[]): void {
+};
+
+/**
+ * Lager en console.table
+ * @param data - array av ukjent data som sendes videre til console.table
+ */
+export const table = (...data: unknown[]): void => {
   if (process.env.NODE_ENV !== 'production') {
     console.table(data);
   }
-}
-export function time(timerName?: string): void {
+};
+
+/**
+ * Lager en console.time
+ * @param timerName - string som sendes videre til console.time
+ */
+export const time = (timerName?: string): void => {
   if (process.env.NODE_ENV !== 'production') {
     console.time(timerName);
   }
-}
-export function timeEnd(timerName?: string): void {
+};
+
+/**
+ * Lager en console.timeEnd
+ * @param timerName - string som sendes videre til console.timeEnd
+ */
+export const timeEnd = (timerName?: string): void => {
   if (process.env.NODE_ENV !== 'production') {
     console.timeEnd(timerName);
   }
-}
-export function trace(message?: any, ...optionalParams: any[]): void {
+};
+
+/**
+ * Lager en console.trace og logger til serveren
+ * @param message - string som sendes i console.trace og til serveren
+ * @param optionalParams params som sendes til console.trace og til serveren
+ */
+export const trace = (message?: string, ...optionalParams: string[]): void => {
   if (process.env.NODE_ENV !== 'production') {
     console.trace(message, optionalParams);
   }
-  logToServer(LogLevel.Trace, message, optionalParams);
-}
-export function warn(message?: any, ...optionalParams: any[]): void {
+  exportFunctions.logToServer(LogLevel.Trace, message, optionalParams);
+};
+
+/**
+ * Lager en console.warn og logger til serveren
+ * @param message - string som sendes i console.warn og til serveren
+ * @param optionalParams params som sendes til console.warn og til serveren
+ */
+export const warn = (message?: string, ...optionalParams: string[]): void => {
   if (process.env.NODE_ENV !== 'production') {
     console.warn(message, optionalParams);
   }
-  logToServer(LogLevel.Warn, message, optionalParams);
-}
+  exportFunctions.logToServer(LogLevel.Warn, message, optionalParams);
+};
 
-export default {
+// This wrapping is necessary for being able to Mock function in jest-tests
+const exportFunctions = {
+  logToServer,
   assert,
   clear,
   count,
@@ -233,4 +344,4 @@ export default {
   warn,
 };
 
-/* eslint-enable @typescript-eslint/no-explicit-any */
+export default exportFunctions;
