3125e65e2beb6368f9202df226182f75cb105182
Marion Le Pontois <marion.le.pontois@ehelse.no>
2020-09-23 09:10:04 +0200

    fikser layout-change unit issue

--- npm/packages/utils/src/layout.ts
+++ npm/packages/utils/src/layout.ts
@@ -2,18 +2,18 @@ import { theme } from '@helsenorge/designsystem-react';
 
 export default {
   isNullToXs: function(): boolean {
-    return window.matchMedia(`screen and (max-width: ${theme.breakpoints.null})`).matches;
+    return window.matchMedia(`screen and (max-width: ${theme.breakpoints.null}px)`).matches;
   },
   isXsToSm: function(): boolean {
-    return window.matchMedia(`screen and (max-width: ${theme.breakpoints.sm})`).matches;
+    return window.matchMedia(`screen and (max-width: ${theme.breakpoints.sm}px)`).matches;
   },
   isSmToMd: function(): boolean {
-    return window.matchMedia(`screen and (max-width: ${theme.breakpoints.md})`).matches;
+    return window.matchMedia(`screen and (max-width: ${theme.breakpoints.md}px)`).matches;
   },
   isMdToLg: function(): boolean {
-    return window.matchMedia(`screen and (max-width: ${theme.breakpoints.lg})`).matches;
+    return window.matchMedia(`screen and (max-width: ${theme.breakpoints.lg}px)`).matches;
   },
   isLgToXl: function(): boolean {
-    return window.matchMedia(`screen and (max-width: ${theme.breakpoints.xl})`).matches;
+    return window.matchMedia(`screen and (max-width: ${theme.breakpoints.xl}px)`).matches;
   },
 };
