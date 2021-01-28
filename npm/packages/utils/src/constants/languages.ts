6f43c689378b4426e5dfb97dc3988280165b1ea2
Frederic Hermandsen <frederic.hermandsen@nhn.no>
2021-01-28 15:23:13 +0100

    oppdatert versjonsnr

--- npm/packages/utils/src/constants/languages.ts
+++ npm/packages/utils/src/constants/languages.ts
@@ -2,4 +2,11 @@ export default {
   NORWEGIAN: 'nb-no',
   NORWEGIAN_NYNORSK: 'nn-no',
   ENGLISH: 'en-US',
+  LITHUANIAN: 'lt-LT',
+  POLISH: 'pl-PL',
+  RUSSIAN: 'ru-RU',
+  ROMANIAN: 'ro-RO',
+  SAMI_NORTHERN: 'se-NO',
 };
+
+export type LanguageLocales = 'nb-no' | 'nn-no' | 'se-no' | 'en-US' | 'lt-LT' | 'pl-PL' | 'ru-RU' | 'ro-RO';
