28238bcf79036f8cd6915a007c030044dc56e0d5
Marion Le Pontois <marion.le.pontois@ehelse.no>
2021-02-17 10:12:17 +0100

    legger til bedre feilhaandtering i focus utils

--- npm/packages/utils/src/focus-utils.tsx
+++ npm/packages/utils/src/focus-utils.tsx
@@ -1,10 +1,17 @@
+import { warn } from './logger';
+
 /**
  * Returnerer document.activeElement (element in focus), uavhengig av om den er i document-dom rllrt shadow-dom
  * @param element HtmlElement som den skal søke i
  */
 export const getDocumentActiveElement = (element: HTMLElement | string): Element | null => {
-  const domNode: TabbableElement =
-    typeof element === 'string' ? (document.querySelector(element) as TabbableElement) : (element as TabbableElement);
-  const root = domNode.getRootNode() as HTMLDocument;
-  return root.activeElement;
+  try {
+    const domNode: TabbableElement =
+      typeof element === 'string' ? (document.querySelector(element) as TabbableElement) : (element as TabbableElement);
+    const root = domNode.getRootNode() as HTMLDocument;
+    return root.activeElement;
+  } catch (e) {
+    warn('Feil ved å ta tak i active element basert på angitt node: ', element, e);
+    return null;
+  }
 };
