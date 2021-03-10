b32555b93a0d315dc2196e39c135503383df6be1
Audun Rundberg <audun.rundberg@nhn.no>
2021-03-10 08:17:27 +0000

    Merged PR 39156: Bruk riktig breapoint i isNullToXs-util
    Lagt til noen tester av disse også så langt det går
    
    Related work items: #247636

--- npm/packages/utils/src/layout.ts
+++ npm/packages/utils/src/layout.ts
@@ -2,7 +2,7 @@ import { theme } from '@helsenorge/designsystem-react';
 
 export default {
   isNullToXs: function(): boolean {
-    return window.matchMedia(`screen and (max-width: ${theme.breakpoints.null}px)`).matches;
+    return window.matchMedia(`screen and (max-width: ${theme.breakpoints.xs}px)`).matches;
   },
   isXsToSm: function(): boolean {
     return window.matchMedia(`screen and (max-width: ${theme.breakpoints.sm}px)`).matches;
