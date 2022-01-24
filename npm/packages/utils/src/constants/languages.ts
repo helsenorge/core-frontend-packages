47934f7ba45c022581b858914d203c2b281ba522
Audun Rundberg <audun@rundberg.no>
2022-01-24 15:23:15 +0100

    fix: toolkit bygger med framework-utils

---
+++ npm/packages/utils/src/constants/languages.ts
@@ -0,0 +1,12 @@
+export enum LanguageLocales {
+  NORWEGIAN = 'nb-no',
+  NORWEGIAN_NYNORSK = 'nn-no',
+  ENGLISH = 'en-GB',
+  LITHUANIAN = 'lt-LT',
+  POLISH = 'pl-PL',
+  RUSSIAN = 'ru-RU',
+  ROMANIAN = 'ro-RO',
+  SAMI_NORTHERN = 'se-NO',
+}
+
+export default LanguageLocales;
