c5e83915e8587b6bdc41469dd7dd10781684f681
Audun Rundberg <audun@rundberg.no>
2022-01-24 14:03:02 +0100

    refactor: flytt hn-utils til framework-utils
    BREAKING CHANGE

--- npm/packages/utils/src/hn-feature-toggle.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Get feature-toggle from window.HN.
- * @param featureName name of feature
- */
-export default function getFeatureToggle(featureName: string): boolean {
-  const getFeatureToggles = window.HN?.PortalCommands?.__GetFeatureToggles__ || window.HN?.Commands?.__GetFeatureToggles__;
-
-  return !!getFeatureToggles?.FeatureToggles?.[featureName];
-}
