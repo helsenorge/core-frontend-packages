c889996e5549de2f4b58b8a164b82e34242da949
Audun Rundberg <audun@rundberg.no>
2022-01-24 14:18:05 +0100

    refactor: flytt web-component utils til framework

--- npm/packages/utils/src/web-component/context.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-
-const EventContext = createContext<Function>(() => null);
-const SubscribeContext = createContext<(eventname: string, callback: Function) => void>((_eventname: string, _callback: Function) => null);
-export const EventProvider = EventContext.Provider;
-export const EventConsumer = EventContext.Consumer;
-export const SubscribeProvider = SubscribeContext.Provider;
-export { EventContext, SubscribeContext };
