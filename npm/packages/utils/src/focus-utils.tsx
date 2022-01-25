1bb5e2328f49574c4b82e1b3d415bff1aafe68db
Audun Rundberg <audun@rundberg.no>
2022-01-25 16:21:58 +0100

    refactor: getDocumentActiveElement tar imot callback for logging til server
    BREAKING CHANGE

--- npm/packages/utils/src/focus-utils.tsx
+++ npm/packages/utils/src/focus-utils.tsx
@@ -3,15 +3,21 @@ import { log } from './logger';
 /**
  * Returnerer document.activeElement (element in focus), uavhengig av om den er i document-dom rllrt shadow-dom
  * @param element HtmlElement som den skal søke i
+ * @param logCallback Funksjon som om det oppstår en feil. Brukes for å logge til server.
  */
-export const getDocumentActiveElement = (element: HTMLElement | string): Element | null => {
+export const getDocumentActiveElement = (
+  element: HTMLElement | string,
+  logCallback?: (message?: string, ...optionalParams: unknown[]) => void
+): Element | null => {
   try {
     const domNode: TabbableElement =
       typeof element === 'string' ? (document.querySelector(element) as TabbableElement) : (element as TabbableElement);
     const root = domNode.getRootNode() as HTMLDocument;
     return root.activeElement;
   } catch (e) {
-    log('Feil ved å ta tak i active element basert på angitt node: ', element, e); // TODO: forut for framework-utils brukte vi error() her som også logget til server
+    const logger = logCallback || log;
+
+    logger('Feil ved å ta tak i active element basert på angitt node: ', element, e);
     return null;
   }
 };
