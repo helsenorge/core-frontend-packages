fae2873bea07d2e0afe38301856aa95d7c9b36e6
SotService <SotService@ehelse.no>
2022-03-03 15:26:56 +0000

    SOT Service update

--- npm/packages/utils/src/types/Resources.d.ts
+++ npm/packages/utils/src/types/Resources.d.ts
@@ -4261,7 +4261,7 @@ Hvis du ikke møter vil du bli belastet for timen.
   */
   persistentMessage_UnsupportedService_Title: string;
   /** 
-  * <span>Meldingen er lagret som utkast. Prøv på nytt senere, eller kontakt behandlingsstedet. Du finner sendte og mottatte meldinger i innboksen.</span>
+  * Meldingen er lagret som utkast. Prøv på nytt senere, eller kontakt behandlingsstedet. Du finner sendte og mottatte meldinger i innboksen.
   */
   clientOperation_NewOfflineAppointment_SendError_Service_Unavailable_Body: string;
   /** 
