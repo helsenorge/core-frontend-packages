38cb9e06b72a7590d879f715e5d9db6ecad0f930
SotService <SotService@ehelse.no>
2022-03-04 13:52:02 +0000

    SOT Service update

--- npm/packages/utils/src/types/Resources.d.ts
+++ npm/packages/utils/src/types/Resources.d.ts
@@ -1132,7 +1132,7 @@ export type HNDialogCommonJS = {
   */
   bookingDisabledText: string;
   /** 
-  * Ingen ledige timer. Du kan velge en annen dag eller måned for å se om det finnes ledige timer. Legekontoret kan også ha timer som ikke kan bestilles her.
+  * Ingen ledige timer. Du kan velge en annen dag eller måned for å se om det finnes ledige timer. Hvis du ikke finner ledige timer på nett, ta kontakt med legekontoret på annen måte.
   */
   bookingEmptyText: string;
   /** 
