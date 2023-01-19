95dc5ac81e4918a5c0089e322796f7898601eee2
Audun Rundberg <audun.rundberg@nhn.no>
2023-01-19 13:51:53 +0100

    test: click outside

--- npm/packages/utils/src/__tests__/click-outside.spec.tsx
+++ npm/packages/utils/src/__tests__/click-outside.spec.tsx
@@ -1,83 +1,54 @@
 import * as React from 'react';
 
-import { mount } from 'enzyme';
-import { act } from 'react-dom/test-utils';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import clickOutside from '../click-outside';
 
-interface CounterProps {
-  counter?: number;
-}
+const onClick = jest.fn();
 
-const Counter: (props: CounterProps) => JSX.Element = ({ counter }: CounterProps) => <h2>{counter}</h2>;
-
-const BasicClickOutsideComponent: React.FunctionComponent<{}> = () => {
-  const [count, setCount] = React.useState(0);
+const BasicClickOutsideComponent: React.FC = () => {
   const wrapperRef = React.useRef<HTMLDivElement>(null);
 
-  const handleClickOnOutsideAction = (): void => {
-    setCount(count + 1);
-  };
-
-  clickOutside(wrapperRef, handleClickOnOutsideAction);
+  clickOutside(wrapperRef, onClick);
 
   return (
-    <div ref={wrapperRef}>
-      {'En funksjon for å registere klikk eller focus utenfor en komponent.'}
-      <Counter counter={count} />
-      <span id="mySpan">{'Klikk/Focus utenfor'}</span>
+    <div>
+      <div ref={wrapperRef}>
+        <button>{'Innenfor'}</button>
+      </div>
+      <button>{'Utenfor'}</button>
     </div>
   );
 };
 
 describe('Click-outside', () => {
-  afterAll(() => {
+  beforeEach(() => {
     jest.clearAllMocks();
   });
 
-  describe('Gitt at det finnes en wrapper med en click counter', () => {
-    describe('Når det klikkes utenfor wrapperen', () => {
-      it('Så kalles det clickOutside og counter øker', () => {
-        const map = {};
-        document.addEventListener = jest.fn((event, callback) => {
-          map[event] = callback;
-        });
+  describe('Gitt at clickOutside brukes i en komponent', () => {
+    describe('Når det klikkes utenfor komponenten', () => {
+      it('Så kalles onClick-handler', async () => {
+        render(<BasicClickOutsideComponent />);
 
-        const wrapper = mount(<BasicClickOutsideComponent />);
+        const button = screen.getByRole('button', { name: 'Utenfor' });
 
-        expect(wrapper.find('h2').text()).toEqual('0');
-        act(() => {
-          map.mousedown({ target: document.createElement('a') });
-        });
+        await userEvent.click(button);
 
-        wrapper.update();
-
-        expect(wrapper.find('h2').text()).toEqual('1');
-        //expect(document.addEventListener).toBeCalledTimes(2); TODO: AUDUN HVA ER GREIA
+        expect(onClick).toHaveBeenCalledTimes(1);
       });
     });
 
-    describe('Når det klikkes inne i wrapperen', () => {
-      it('Så kalles det clickCounter og counter øker ikke', () => {
-        const map = {};
-        document.addEventListener = jest.fn((event, callback) => {
-          map[event] = callback;
-        });
+    describe('Når det klikkes innenfor komponenten', () => {
+      it('Så kalles ikke onClick-handler', async () => {
+        render(<BasicClickOutsideComponent />);
 
-        const wrapper = mount(<BasicClickOutsideComponent />);
+        const button = screen.getByRole('button', { name: 'Innenfor' });
 
-        expect(wrapper.find('h2').text()).toEqual('0');
+        await userEvent.click(button);
 
-        act(() => {
-          const child = document.createElement('span');
-          wrapper.getDOMNode().appendChild(child);
-          map.mousedown({ target: child });
-        });
-        wrapper.update();
-
-        expect(wrapper.find('h2').text()).toEqual('0');
-
-        expect(document.addEventListener).toBeCalledTimes(1);
+        expect(onClick).not.toHaveBeenCalled();
       });
     });
   });
