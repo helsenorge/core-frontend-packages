b13c6b9b23695a0284b9ab568f33f80fdd69c49a
Audun Rundberg <audun@rundberg.no>
2023-02-04 11:18:24 +0100

    feat(eslint-config): dangerouslySetInnerHTML gir error

---
+++ npm/packages/eslint-config/rules.js
@@ -0,0 +1,112 @@
+module.exports = {
+  'react/no-danger': 'error',
+  'prettier/prettier': 'error',
+  'arrow-body-style': 'off',
+  'prefer-arrow-callback': 'off',
+  '@typescript-eslint/ban-types': 'warn',
+  '@typescript-eslint/explicit-function-return-type': 'warn',
+  '@typescript-eslint/no-empty-interface': 'warn',
+  '@typescript-eslint/no-explicit-any': 'error',
+  'react/jsx-no-literals': 'error',
+  'react/display-name': 'warn',
+  'no-console': 'error',
+  'no-debugger': 'error',
+  'import/order': [
+    'warn',
+    {
+      groups: ['builtin', 'external', 'type', 'internal', ['parent', 'sibling'], 'index'],
+      pathGroups: [
+        {
+          pattern: 'react',
+          group: 'external',
+          position: 'before',
+        },
+        {
+          pattern: 'HNResources',
+          group: 'external',
+          position: 'after',
+        },
+        {
+          pattern: 'HNEntities/**',
+          group: 'external',
+          position: 'after',
+        },
+        {
+          pattern: 'types/**',
+          group: 'type',
+        },
+        {
+          pattern: '../types/**',
+          group: 'type',
+        },
+        {
+          pattern: '../../types/**',
+          group: 'type',
+        },
+        {
+          pattern: '../../../types/**',
+          group: 'type',
+        },
+        {
+          pattern: '../../../../types/**',
+          group: 'type',
+        },
+        {
+          pattern: '../../../../../types/**',
+          group: 'type',
+        },
+        {
+          pattern: '../../../../../../types/**',
+          group: 'type',
+        },
+        {
+          pattern: '../../../../../../../types/**',
+          group: 'type',
+        },
+        {
+          pattern: '../../../../../../../../types/**',
+          group: 'type',
+        },
+        {
+          pattern: '../../../../../../../../../types/**',
+          group: 'type',
+        },
+        {
+          pattern: '../../../../../../../../../../types/**',
+          group: 'type',
+        },
+        {
+          pattern: '@helsenorge/designsystem-react/**',
+          group: 'internal',
+          position: 'before',
+        },
+        {
+          pattern: '@helsenorge/toolkit/**',
+          group: 'internal',
+          position: 'before',
+        },
+        {
+          pattern: '@helsenorge/**',
+          group: 'internal',
+          position: 'before',
+        },
+        {
+          pattern: './*.scss',
+          group: 'sibling',
+          position: 'after',
+        },
+        {
+          pattern: './*.css',
+          group: 'sibling',
+          position: 'after',
+        },
+      ],
+      pathGroupsExcludedImportTypes: ['react'],
+      'newlines-between': 'always',
+      alphabetize: {
+        order: 'asc',
+        caseInsensitive: true,
+      },
+    },
+  ],
+};
