672f2750629eddfe2732c52519d603c7c38717e4
Audun Rundberg <audun.rundberg@nhn.no>
2023-03-16 08:59:57 +0000

    Merged PR 61501: feat(eslint-config,core-build): stylelint for linting av css/scss
    Related work items: #210178

---
+++ npm/packages/eslint-config/stylelint.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: 'stylelint-config-standard-scss',
+  rules: {
+    'function-name-case': [
+      'lower',
+      {
+        ignoreFunctions: ['getSpacer'],
+      },
+    ],
+    'unit-no-unknown': [
+      true,
+      {
+        ignoreUnits: ['4xs', '3xs', '2xs', 'xs', 's', 'm', 'l', 'xl', '2xl', '3xl', '4xl', '5xl', '6xl'],
+      },
+    ],
+  },
+};
