1d229b62184b93e0749e61ed86931314f0741d75
Audun Rundberg <audun.rundberg@nhn.no>
2023-05-30 12:44:14 +0000

    Merged PR 64111: chore(core-build,library-build): bruk plugin-transform-object-rest-spread
    Related work items: #303447

--- npm/packages/library-build/config/babel.config.js
+++ npm/packages/library-build/config/babel.config.js
@@ -20,7 +20,7 @@ const parentBabelConfig = {
   plugins: [
     '@babel/plugin-syntax-dynamic-import',
     ['@babel/plugin-proposal-class-properties', { loose: true }],
-    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-transform-object-rest-spread',
   ],
 };
 
