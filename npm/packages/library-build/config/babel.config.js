04b4c39fd8107707192e704182a28f6f7ec09676
audunru <audun.rundberg@nhn.no>
2024-03-05 14:41:55 +0100

    build: fjern mer av webpack

--- npm/packages/library-build/config/babel.config.js
+++ npm/packages/library-build/config/babel.config.js
@@ -1,19 +1,6 @@
-const browserslistFromPackageJson = require('./../package.json').browserslist;
-
 const parentBabelConfig = {
   presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-          browsers: browserslistFromPackageJson,
-        },
-        useBuiltIns: 'usage',
-        corejs: 3,
-        loose: true,
-      },
-    ],
+    ['@babel/preset-env', { targets: { node: 'current' } }],
     ['@babel/preset-react', { typescript: true }],
     ['@babel/preset-typescript', { allExtensions: true, isTSX: true }],
   ],
