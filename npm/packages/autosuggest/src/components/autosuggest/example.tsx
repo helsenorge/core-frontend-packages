53e38c5d2ec992ccaf151f721d91457d07839b62
audunru <audun.rundberg@nhn.no>
2024-03-19 09:50:54 +0100

    docs: autosuggest fc

--- npm/packages/autosuggest/src/components/autosuggest/example.tsx
+++ npm/packages/autosuggest/src/components/autosuggest/example.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import * as React from 'react';
 
 import {
@@ -12,79 +13,63 @@ import Spacer from '@helsenorge/designsystem-react/components/Spacer';
 
 import Autosuggest, { Suggestion } from '.';
 
-export class AutosuggestExample extends React.Component<{}, { suggestions: Array<Suggestion>; value: string; error: boolean }> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      suggestions: [],
-      value: '',
-      error: false,
-    };
-  }
+export const AutosuggestExample: React.FC = () => {
+  const [suggestions, setSuggestions] = React.useState<Suggestion[]>([]);
+  const [value, setValue] = React.useState<string>('');
+  const [error, setError] = React.useState<boolean>(false);
 
-  onChange = (e: React.FormEvent<HTMLInputElement>, { newValue }: { newValue: string; method: string }): void => {
+  const onChange = (e: React.FormEvent<HTMLInputElement>, { newValue }: { newValue: string; method: string }): void => {
     console.log('onChange', e, newValue);
-    this.setState({ value: newValue });
+    setValue(newValue);
   };
 
-  onSuggestionsFetchRequested: SuggestionsFetchRequested = (requestParams: SuggestionsFetchRequestedParams) => {
+  const onSuggestionsFetchRequested: SuggestionsFetchRequested = (requestParams: SuggestionsFetchRequestedParams) => {
     console.log('onSuggestionsFetchRequested', requestParams.value);
-    const suggestions: Array<Suggestion> = [
+
+    setSuggestions([
       { label: 'First suggestion', value: 'First suggestion' },
       { label: 'Second suggestion', value: 'Second suggestion', optionalLabel: 'this is my optional label' },
       { label: 'Third suggestion', value: 'Third suggestion' },
       { label: 'Fourth suggestion', value: 'Fourth suggestion' },
-    ];
-
-    this.setState({
-      suggestions,
-    });
+    ]);
   };
 
-  onSuggestionsSelected: OnSuggestionSelected<Suggestion> = (e, data) => {
+  const onSuggestionsSelected: OnSuggestionSelected<Suggestion> = (e, data) => {
     console.log('onSuggestionsSelected', data.suggestion, e);
 
-    this.setState({
-      value: data.suggestion.value,
-    });
+    setValue(data.suggestion.value);
   };
 
-  onSuggestionsClearRequested: OnSuggestionsClearRequested = () => {
+  const onSuggestionsClearRequested: OnSuggestionsClearRequested = () => {
     console.log('onSuggestionsClearRequested');
 
-    this.setState({
-      suggestions: [],
-    });
+    setSuggestions([]);
   };
 
-  toggleError = () =>
-    this.setState({
-      error: !this.state.error,
-    });
+  const toggleError = (): void => setError(!error);
 
-  render() {
-    return (
-      <>
-        <Button onClick={this.toggleError}>{'Feil på/av'}</Button>
-        <Spacer />
-        <Autosuggest
-          inputProps={{
-            id: 'id1',
-            type: 'search',
-            value: this.state.value,
-            onChange: this.onChange,
-          }}
-          label={'Søk i behandlinger og undersøkelser'}
-          suggestions={this.state.suggestions}
-          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-          onSuggestionSelected={this.onSuggestionsSelected}
-          error={this.state.error}
-          errorText={this.state.error ? 'Du må skrive minst 3 tegn' : undefined}
-        />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Spacer />
+      <Button onClick={toggleError}>{'Feil på/av'}</Button>
+      <Spacer />
+      <Autosuggest
+        inputProps={{
+          id: 'id1',
+          type: 'search',
+          value: value,
+          onChange: onChange,
+        }}
+        label={'Søk i behandlinger og undersøkelser'}
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={onSuggestionsClearRequested}
+        onSuggestionSelected={onSuggestionsSelected}
+        error={error}
+        errorText={error ? 'Du må skrive minst 3 tegn' : undefined}
+      />
+    </>
+  );
+};
 
 export default AutosuggestExample;
