61d71608a907df9e1a37e3d8a08b835aa03752a1
Audun Rundberg <audun.rundberg@nhn.no>
2024-04-15 09:22:16 +0000

    Merged PR 79452: feat: ny hendelsesinngang i header og på forsiden
    Related work items: #321008

---
+++ npm/packages/utils/src/__tests__/object-utils.spec.ts
@@ -0,0 +1,115 @@
+import { merge } from '../object-utils';
+
+describe('merge', () => {
+  describe('Gitt at a og b er to forskjellige objekter', () => {
+    describe('Når merge kalles', () => {
+      it('Så returnerer den a og b slått sammen', () => {
+        const merged = merge({ a: 'foo' }, { b: 'bar' });
+
+        expect(merged).toEqual({
+          a: 'foo',
+          b: 'bar',
+        });
+      });
+    });
+  });
+  describe('Gitt at a har en string-verdi i argument 1', () => {
+    describe('Gitt at a er undefined i argument 2', () => {
+      describe('Når merge kalles', () => {
+        it('Så returnerer den a-stringen fra argument 1 ', () => {
+          const merged = merge({ a: 'foo' }, { a: undefined, b: 'bar', c: 'bar' });
+
+          expect(merged).toEqual({
+            a: 'foo',
+            b: 'bar',
+            c: 'bar',
+          });
+        });
+      });
+    });
+    describe('Gitt at a er null i argument 2', () => {
+      describe('Når merge kalles', () => {
+        it('Så returnerer den a-stringen fra argument 1 ', () => {
+          const merged = merge({ a: 'foo' }, { a: null, b: 'bar', c: 'bar' });
+
+          expect(merged).toEqual({
+            a: 'foo',
+            b: 'bar',
+            c: 'bar',
+          });
+        });
+      });
+    });
+    describe('Gitt at a ikke eksisterer i argument 2', () => {
+      describe('Når merge kalles', () => {
+        it('Så returnerer den a-stringen fra argument 1 ', () => {
+          const merged = merge({ a: 'foo' }, { b: 'bar', c: 'bar' });
+
+          expect(merged).toEqual({
+            a: 'foo',
+            b: 'bar',
+            c: 'bar',
+          });
+        });
+      });
+    });
+  });
+  describe('Gitt at a har en objekt-verdi i argument 1', () => {
+    describe('Gitt at a er undefined i argument 2', () => {
+      describe('Når merge kalles', () => {
+        it('Så returnerer den a-objektet fra argument 1 ', () => {
+          const merged = merge({ a: { header: 'foo' } }, { a: undefined, b: 'bar', c: 'bar' });
+
+          expect(merged).toEqual({
+            a: { header: 'foo' },
+            b: 'bar',
+            c: 'bar',
+          });
+        });
+      });
+    });
+
+    describe('Gitt at a er null i argument 2', () => {
+      describe('Når merge kalles', () => {
+        it('Så returnerer den a-objektet fra argument 1 ', () => {
+          const merged = merge({ a: { header: 'foo' } }, { a: null, b: 'bar', c: 'bar' });
+
+          expect(merged).toEqual({
+            a: { header: 'foo' },
+            b: 'bar',
+            c: 'bar',
+          });
+        });
+      });
+    });
+
+    describe('Gitt at a ikke eksisterer i argument 2', () => {
+      describe('Når merge kalles', () => {
+        it('Så returnerer den a-objektet fra argument 1 ', () => {
+          const merged = merge({ a: { header: 'foo' } }, { b: 'bar', c: 'bar' });
+
+          expect(merged).toEqual({
+            a: { header: 'foo' },
+            b: 'bar',
+            c: 'bar',
+          });
+        });
+      });
+    });
+  });
+  describe('Gitt at a har en objekt-verdi i argument 1 med flere underverdier', () => {
+    describe('Gitt at a har en objekt-verdi i argument 2 med flere underverdier', () => {
+      describe('Når merge kalles', () => {
+        it('Så returnerer den a-objektet fra argument 1 ', () => {
+          const merged = merge({ a: { header: 'foo' } }, { a: { header: undefined, footer: 'bar' }, b: 'bar', c: 'bar' });
+
+          expect(merged).toEqual({
+            a: { header: 'foo', footer: 'bar' },
+            b: 'bar',
+            c: 'bar',
+          });
+        });
+      });
+    });
+  });
+});
---
+++ npm/packages/utils/src/object-utils.ts
@@ -0,0 +1,22 @@
+/**
+ * Slår sammen to objekter rekursivt.
+ *
+ * @param defaults Objekt med verdier som vil brukes som fallback
+ * @param overrides Objekt med verdier som vil overstyre defaults
+ * @returns
+ */
+export const merge = <D extends object, O extends object>(defaults: D, overrides: O): D & O => {
+  const merged: D & O = { ...defaults, ...overrides };
+
+  for (const key in merged) {
+    if (typeof defaults[key] === 'object' && !Array.isArray(defaults[key]) && defaults[key] !== null) {
+      merged[key] = merge(defaults[key], overrides[key]);
+    } else {
+      if ((!overrides || !overrides[key]) && !!defaults[key]) {
+        merged[key] = defaults[key];
+      }
+    }
+  }
+
+  return merged;
+};
