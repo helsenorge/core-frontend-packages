511490dde5e4790d4330e0df160e8f5f5206c198
Audun Rundberg (Innleid) <audun.rundberg@nhn.no>
2025-05-05 08:05:49 +0000

    Merged PR 131258: feat(eslint-config): gjenbrukbar prettier-config
    Related work items: #351061

--- npm/packages/eslint-config/prettier.config.js
+++ npm/packages/eslint-config/prettier.config.js
@@ -1,7 +1,9 @@
-module.exports = {
+/** @type {import("prettier").Config} */
+export default {
   arrowParens: 'avoid',
   proseWrap: 'always',
   trailingComma: 'es5',
   singleQuote: true,
   printWidth: 140,
+  endOfLine: 'auto',
 };
---
+++ npm/packages/utils/prettier.config.js
@@ -0,0 +1,3 @@
+import config from '@helsenorge/eslint-config/prettier.config.js';
+
+export default config;
