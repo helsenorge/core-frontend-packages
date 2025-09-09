4f4cd06f7e7d4b282ff8950819165de3ecec6202
Audun Rundberg <audun.rundberg@nhn.no>
2025-09-09 15:03:02 +0200

    refactor(core-utils): slett querystring og referrer-utils

--- npm/packages/utils/src/referrer-utils.ts
+++
@@ -1,6 +0,0 @@
-import { getQueryStrings } from './querystring-utils';
-
-export const isReferrer = (referrer: string): boolean => {
-  const queryStrings = getQueryStrings() as { referrer?: string };
-  return queryStrings && queryStrings.referrer === referrer;
-};
