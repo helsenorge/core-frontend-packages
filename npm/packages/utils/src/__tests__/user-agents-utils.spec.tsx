f7e9c4aa46b0374b7e2c3e221a1ff6343ac1e57c
Audun Rundberg <audun.rundberg@nhn.no>
2025-10-06 12:42:42 +0200

    style: ts-feil i tester

--- npm/packages/utils/src/__tests__/user-agents-utils.spec.tsx
+++ npm/packages/utils/src/__tests__/user-agents-utils.spec.tsx
@@ -1,28 +1,11 @@
-import { afterAll, beforeAll, describe, expect, it, vi } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 
 import { isMobileUA } from '../user-agents-utils';
 
 describe('User-agents-utils', () => {
-  const originalUserAgent = global.navigator.userAgent;
-  beforeAll(() => {
-    Object.defineProperty(
-      window.navigator,
-      'userAgent',
-      (value => ({
-        get() {
-          return value;
-        },
-        set(v) {
-          value = v;
-        },
-      }))(window.navigator['userAgent'])
-    );
-  });
-
   afterAll(() => {
-    vi.clearAllMocks();
+    vi.unstubAllGlobals();
   });
-
   describe('Gitt at window.navigator er uendret', () => {
     describe('Når isMobileUA kalles', () => {
       it('Så returnerer den false', () => {
@@ -35,12 +18,13 @@ describe('User-agents-utils', () => {
   describe('Gitt at window.navigator er satt til android', () => {
     describe('Når isMobileUA kalles', () => {
       it('Så returnerer den true', () => {
-        global.navigator.userAgent =
-          'Mozilla/5.0 (Linux; Android 5.1.1; SM-N750K Build/LMY47X; ko-kr) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Mobile Safari/537.36 Puffin/6.0.8.15804AP';
+        vi.stubGlobal('navigator', {
+          userAgent:
+            'Mozilla/5.0 (Linux; Android 5.1.1; SM-N750K Build/LMY47X; ko-kr) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Mobile Safari/537.36 Puffin/6.0.8.15804AP',
+        });
 
         const mobile = isMobileUA();
         expect(mobile).toBeTruthy();
-        global.navigator.userAgent = originalUserAgent;
       });
     });
   });
