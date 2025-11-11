f08d9d331c2f0c3a405b521fbc5454b0b7a11792
Audun Rundberg <audun.rundberg@nhn.no>
2025-11-11 16:10:52 +0100

    fix(core-build): mock fetch med node-fetch under tester

--- npm/packages/library-build/src/setupTests.vite.ts
+++ npm/packages/library-build/src/setupTests.vite.ts
@@ -1,5 +1,19 @@
+import fetchPolyfill, { Request as RequestPolyfill } from 'node-fetch';
 import { vi } from 'vitest';
 import '@testing-library/jest-dom/vitest';
+
+// @todo: Fjern etter oppdatering til vitest 4, fjern da også dependency til node-fetch
+// https://github.com/reduxjs/redux-toolkit/issues/4966#issuecomment-3115230061
+Object.defineProperty(global, 'fetch', {
+  // MSW will overwrite this to intercept requests
+  writable: true,
+  value: fetchPolyfill,
+});
+
+Object.defineProperty(global, 'Request', {
+  writable: false,
+  value: RequestPolyfill,
+});
 
 declare global {
   interface Window {
